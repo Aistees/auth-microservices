67705552d623049d27869f014f95997903dd7093
corentin-roussel <corentin.roussel@laplateforme.io>
2025-12-19 08:18:11 +0100

    change single quote for douuble quote

--- auth-back/src/app.module.ts
+++ auth-back/src/app.module.ts
@@ -1,8 +1,8 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { PrismaModule } from '../prisma/prisma.module';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { AuthModule } from "./auth/auth.module";
+import { PrismaModule } from "./prisma/prisma.module";
 
 @Module({
   imports: [AuthModule, PrismaModule],
