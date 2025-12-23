4ef6435835d48c023b64b0f6b503d47ad9189475
corentin-roussel <corentin.roussel@laplateforme.io>
2025-12-23 18:47:29 +0100

    add generated primsa

--- auth-back/src/generated/prisma/internal/class.ts
+++ auth-back/src/generated/prisma/internal/class.ts
@@ -17,8 +17,8 @@ import type * as Prisma from "./prismaNamespace"
 
 const config: runtime.GetPrismaClientConfig = {
   "previewFeatures": [],
-  "clientVersion": "7.1.0",
-  "engineVersion": "ab635e6b9d606fa5c8fb8b1a7f909c3c3c1c98ba",
+  "clientVersion": "7.2.0",
+  "engineVersion": "0c8ef2ce45c83248ab3df073180d5eda9e8be7a3",
   "activeProvider": "postgresql",
   "inlineSchema": "// This is your Prisma schema file,\n// learn more about it in the docs: https://pris.ly/d/prisma-schema\n\n// Looking for ways to speed up your queries, or scale easily with your serverless or edge functions?\n// Try Prisma Accelerate: https://pris.ly/cli/accelerate-init\n\ngenerator client {\n  provider = \"prisma-client\"\n  output   = \"../src/generated/prisma\"\n}\n\ndatasource db {\n  provider = \"postgresql\"\n}\n\nmodel User {\n  id       String   @id @default(uuid())\n  login    String   @unique\n  password String\n  roles    String[]\n  status   String\n}\n",
   "runtimeDataModel": {
