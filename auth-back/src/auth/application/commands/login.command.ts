842fb59e238491eee5aaa48ef6d55fa1af96a2d0
corentin-roussel <corentin.roussel@laplateforme.io>
2025-12-23 19:26:12 +0100

    build constructor for regiter login refresh-token

---
+++ auth-back/src/auth/application/commands/login.command.ts
@@ -0,0 +1,6 @@
+export class LoginCommand {
+    constructor(
+        public readonly login: string,
+        public readonly password: string,
+    ) {}
+}
---
+++ auth-back/src/auth/application/commands/register.command.ts
@@ -0,0 +1,8 @@
+export class RegisterCommand {
+    constructor(
+        public readonly login: string,
+        public readonly password: string,
+        public readonly roles: string[],
+        public readonly status: string,
+    ) {}
+}
