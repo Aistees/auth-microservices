bde8ed58d87e5c1002ca42d9691ec6bf6feee136
corentin-roussel <corentin.roussel@laplateforme.io>
2025-12-23 19:23:42 +0100

    change return of a function

--- auth-back/src/auth/domain/ports/repositories/user.repository.port.ts
+++ auth-back/src/auth/domain/ports/repositories/user.repository.port.ts
@@ -1,7 +1,7 @@
 import { User } from '../../entities/user.entity';
 
 export abstract class UserRepositoryPort {
-  abstract findByLogin(login: string): Promise<User | null>;
+  abstract findByLogin(login: string): Promise<User>;
   abstract findById(user_id: string): Promise<User | null>;
   abstract save(user: User): Promise<void>;
   abstract update(user_id: string, updatedUser: User): Promise<void>;
