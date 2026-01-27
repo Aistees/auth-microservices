5451bcce35272fcba5bb2f127ad2bc70092d4557
corentin.roussel <corentin.roussel@laplateforme.io>
2026-01-27 21:34:43 +0100

    app

--- cinema-microservice/src/app.module.ts
+++ cinema-microservice/src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { CinemaController } from './interfaces/controllers/cinema.controller';
+import { CreateCinemaUseCase } from './application/use-cases/create-cinema.use-case';
+import { ListCinemasUseCase } from './application/use-cases/list-cinemas.use-case';
+import { GetCinemaUseCase } from './application/use-cases/get-cinema.use-case';
+import { DeleteCinemaUseCase } from './application/use-cases/delete-cinema.use-case';
+import { UpdateCinemaUseCase } from './application/use-cases/update-cinema.use-case';
+import { PrismaCinemaRepository } from './infrastructure/persistence/repositories/prisma.cinema.repository';
+import { CinemaRepositoryPort } from './domain/ports/cinema.port';
+import { PrismaService } from './infrastructure/persistence/prisma.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [CinemaController],
+  providers: [
+    PrismaService,
+    CreateCinemaUseCase,
+    ListCinemasUseCase,
+    GetCinemaUseCase,
+    DeleteCinemaUseCase,
+    UpdateCinemaUseCase,
+    {
+      provide: CinemaRepositoryPort,
+      useClass: PrismaCinemaRepository,
+    },
+  ],
 })
 export class AppModule {}
