cfa1d509a1e346c68fffb9a95dfdcb9ab3393900
corentin.roussel <corentin.roussel@laplateforme.io>
2026-02-06 22:13:53 +0100

    update package json , update schema prisma and prisma service and add swagger to cinema microservices

--- cinema-microservice/src/infrastructure/persistence/prisma.service.ts
+++ cinema-microservice/src/infrastructure/persistence/prisma.service.ts
@@ -1,6 +1,10 @@
 // src/infrastructure/persistence/prisma.service.ts
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from '../../../generated/prisma/client';
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
+
+// Load the Prisma client from generated folder
+const { PrismaClient } = require('../../../generated/prisma/client');
 
 @Injectable()
 export class PrismaService 
@@ -9,7 +13,13 @@ export class PrismaService
 {
   // Optional: Configure logs or connection URL here if needed
   constructor() {
-    super({ log: ['warn', 'error'], accelerateUrl: '' });
+    // 👇 Prisma 7 Setup: Create the connection pool and adapter
+    const connectionString = `${process.env.DATABASE_URL}`;
+    const pool = new Pool({ connectionString });
+    const adapter = new PrismaPg(pool);
+
+    // Pass the adapter to the parent constructor
+    super({ adapter });
   }
 
   async onModuleInit() {
--- cinema-microservice/src/main.ts
+++ cinema-microservice/src/main.ts
@@ -1,8 +1,33 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Cinema Microservice')
+    .setDescription('The Cinema Management API based on Clean Architecture')
+    .setVersion('1.0')
+    .addBearerAuth({
+      type: 'http', 
+        scheme: 'bearer', 
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT Token',
+        in: 'header',
+    },
+    'JwtAuthGuard'
+  )
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
